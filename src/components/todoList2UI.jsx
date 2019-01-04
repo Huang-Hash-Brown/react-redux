5b8956f923aaf8c1549b54574341f850d92040cd
zhmail0830163co <zhmail0830@163.com>
2019-01-04 20:39:08 +0800

    change UI component to None state component

--- src/components/todoList2UI.jsx
+++ src/components/todoList2UI.jsx
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Input, Button, List } from 'antd';
-
-class TodoListUI extends Component {
 // 该只负责UI的渲染(傻瓜组件)
-  render() {
-    return (
-      <div style={{ margin: '10px 10px' }}>
-        <Input
-          placeholder="todo info"
-          style={{ width: 300, marginRight: 10 }}
-          value={ this.props.inputValue }
-          onChange={ this.props.hanldeInputChange }
-        />
-        <Button type="primary" onClick={this.props.handleSubmit}>提交</Button>
-        <List
-          style={{ marginTop: 10, width: 300 }}
-          bordered
-          dataSource={this.props.list}
-          renderItem={(item, index) => (<List.Item onClick={ () => { this.props.handleDelete(index)} }>{item}</List.Item>)}
-        />
-      </div>
-    )
-  }
+// 可封装为 无状态 组件  无状态函数组件性能 > es6 Class组件性能
+const TodoListUI = (props) => {
+  return (
+    <div style={{ margin: '10px 10px' }}>
+      <Input
+        placeholder="todo info"
+        style={{ width: 300, marginRight: 10 }}
+        value={ props.inputValue }
+        onChange={ props.hanldeInputChange }
+      />
+      <Button type="primary" onClick={props.handleSubmit}>提交</Button>
+      <List
+        style={{ marginTop: 10, width: 300 }}
+        bordered
+        dataSource={props.list}
+        renderItem={(item, index) => (<List.Item onClick={ () => { props.handleDelete(index)} }>{item}</List.Item>)}
+      />
+    </div>
+  )
 }
 
 export default TodoListUI;
